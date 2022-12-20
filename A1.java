4f872fefe75d41d5a122ae433819cfb9409f5486
Akshay <deshpanderakshay@gmail.com>
2022-12-20 22:39:58 +0530

    file uploaded

---
+++ A1.java
@@ -0,0 +1,19 @@
+ class A1
+{
+ void show()
+ {
+  System.out.println("akshay");
+ }
+ void show(int a)
+ {
+  System.out.println("shree");
+ }
+ public class OverLoad{
+ public static void main(String[] args)
+ {
+   A1 a=new A1(); 
+   a.show();
+   a.show(10);
+ }
+}  
+}
